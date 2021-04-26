13a11e47c0bf2161eaf11e0e554c3f4836943027
Anteste <ialami2006@gmail.com>
2021-04-26 09:21:21 +0000

    feat: Setup PinsController, Add Psysh Bundle, Add Pluralize twig function

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,10 @@ class PinsController extends AbstractController
     /**
      * @Route("/", name="pins")
      */
-    public function index(): Response
+    public function index(PinRepository $pinRepository): Response
     {
-        return $this->render('pins/index.html.twig');
+        $pins = $pinRepository->findAll();
+
+        return $this->render('pins/index.html.twig', compact('pins'));
     }
 }
---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFilter('filter_name', [$this, 'doSomething']),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('pluralize', [$this, 'doSomething']),
+        ];
+    }
+
+    public function doSomething(int $count, string $singular, ?string $plural = null)
+    {
+        $plural = $plural ?? $singular . 's';
+
+        if ($count == 1) {
+            return $singular;
+        } else {
+            return $plural;
+        }
+    }
+}
